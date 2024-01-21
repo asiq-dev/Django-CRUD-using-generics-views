b0f3ecea6d9a4851b2349ac01f26a31f484d8272
Asiqul Islam <asiq.dev@gmail.com>
2024-01-22 03:24:01 +0600

    Django-CRUD-using-generics-views

---
+++ mysite/webapp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from webapp.models import Employee
+from webapp.forms import EmployeeModelForm
+
+# Create your views here.
+
+class EmployeeListView(ListView):
+    model = Employee
+    template_name = 'home.html'
+    context_object_name = 'employee_list'
+    success_url = '/'
+
+class EmployeeCreateView(CreateView):
+    model = Employee
+    form_class  = EmployeeModelForm
+    template_name = 'employee_form.html'
+    success_url = '/'
+
+class EmployeeUpdateView(UpdateView):
+    model = Employee
+    form_class = EmployeeModelForm
+    template_name = 'employee_form.html'
+    success_url = '/'
+
+class EmployeeDeleteView(DeleteView):
+    model = Employee
+    success_url = '/'
+
+    def get(self, request, *args, **kwargs):
+        return self.delete(request, *args, **kwargs)
